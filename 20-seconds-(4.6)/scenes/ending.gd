381043594478bf94c037a55078c1e5823533142e
cpinta <pinta.christian@outlook.com>
2026-04-03 21:25:55 -0500

    added audio to ending

--- 20-seconds-(4.6)/scenes/ending.gd
+++ 20-seconds-(4.6)/scenes/ending.gd
@@ -2,12 +2,23 @@ extends CanvasLayer
 class_name Ending
 
 var colorRect: ColorRect
+var audio: AudioStreamPlayer
+var pengAudio: AudioStreamPlayer
+
+var ambience: AudioStream = preload("res://audio/ending/freesound_community-restaurant_ambence-53039.mp3")
+var pengEat: AudioStream = preload("res://audio/ending/end peng chomp.mp3")
 
 var inputTimer: float = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	colorRect = $ColorRect
+	audio = $AudioStreamPlayer
+	audio.stream = ambience
+	audio.play()
+	pengAudio = $pengaudio
+	pengAudio.stream = pengEat
+	pengAudio.play()
 	pass # Replace with function body.
 
 signal ended()
