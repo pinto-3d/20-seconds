fbcf5a8895ded16e05ed51791b7e86ce575d1171
cpinta <pinta.christian@outlook.com>
2026-04-03 21:13:57 -0500

    fixed a button prompt. added input delay to skipping ending

--- 20-seconds-(4.6)/scenes/ending.gd
+++ 20-seconds-(4.6)/scenes/ending.gd
@@ -3,6 +3,8 @@ class_name Ending
 
 var colorRect: ColorRect
 
+var inputTimer: float = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	colorRect = $ColorRect
@@ -15,7 +17,10 @@ func _process(delta: float) -> void:
 	
 	if colorRect.color.a > 0:
 		colorRect.color.a -= delta
-	if Input.is_action_just_pressed("advance_text") || Input.is_action_just_pressed("pause"):
-		ended.emit()
-		pass
+	if inputTimer > 0:
+		inputTimer -= delta
+	else:
+		if Input.is_action_just_pressed("advance_text") || Input.is_action_just_pressed("pause"):
+			ended.emit()
+			pass
 	pass
